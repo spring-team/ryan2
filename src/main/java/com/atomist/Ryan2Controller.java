acbf0c9fb933578255027fcec2d3c1336e5c8c66
Rod Johnson <johnsonroda@gmail.com>
2018-02-13 05:44:15 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/Ryan2Controller.java
@@ -0,0 +1,17 @@
+package com.atomist;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+class Ryan2Controller {
+
+    @RequestMapping(method = GET, path = "hello/{name}")
+    public String person(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+}
